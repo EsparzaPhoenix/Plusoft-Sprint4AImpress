71929beeaf8660ec74ca0af749ff5087e535c490
EsparzaPhoenix <esparza_leonardo@hotmail.com>
2024-11-02 21:52:19 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/aimpress/AimpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aimpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimpressApplication.class, args);
+	}
+
+}
